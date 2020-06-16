f4cffc0b623c4477383aae1691eae37397fe9e8e
CabosManuel <cabos.apolaya@gmail.com>
2020-06-15 19:46:25 -0500

    [Correcciones]

--- AppMovilNivelBasico/app/src/main/java/idat/edu/pe/appmovilnivelbasico/Ejercicio23Activity.java
+++ AppMovilNivelBasico/app/src/main/java/idat/edu/pe/appmovilnivelbasico/Ejercicio23Activity.java
@@ -2,7 +2,6 @@ package idat.edu.pe.appmovilnivelbasico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
